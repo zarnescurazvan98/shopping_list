fc4750aca659898d7aed802259902cb45944604e
Razvan Zarnescu <zarnescurazvan98@gmail.com>
2021-09-26 21:21:22 +0300

    Updates

---
+++ precache-manifest.5a262677659bfaf7f7ea3c927f3d4ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf16c3d2322a9c224699c39a2ee936a5",
+    "url": "/shopping_list/index.html"
+  },
+  {
+    "revision": "43fd45947b24c4639812",
+    "url": "/shopping_list/static/css/main.6829a2eb.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/shopping_list/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping_list/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fd45947b24c4639812",
+    "url": "/shopping_list/static/js/main.c36f42ee.chunk.js"
+  },
+  {
+    "revision": "b3b4382d0f163ad730b5",
+    "url": "/shopping_list/static/js/runtime-main.74c8d21b.js"
+  }
+]);
